162972bbdd73115c1c85419f87ab5fbef389f641
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2021-08-31 05:13:06 +0900

    feat: Add methods that lock properties

--- src/shape.ts
+++ src/shape.ts
@@ -1,11 +1,13 @@
 export type TransformProps = {
   target: HTMLElement;
   apply: (vars: gsap.TweenVars) => void;
+  getProperty: (key: keyof gsap.TweenVars) => gsap.TweenValue;
+  lock: (key: keyof gsap.TweenVars, value: gsap.TweenValue) => void;
+  unlock: (key: keyof gsap.TweenVars) => void;
+  isLocked: (key: keyof gsap.TweenVars) => boolean;
 };
 
-export type RestoreProps = {
-  apply: (vars: gsap.TweenVars) => void;
-};
+export type RestoreProps = Omit<TransformProps, "target">;
 
 export type ShapeConstructorProps = {
   pointer: HTMLElement;
